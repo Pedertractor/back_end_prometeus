f7be6f7ef87fe744c01f0da62cbec114e47efcaa
Pedro Henrique Prado <pedroh.prado@outlook.com>
2024-08-01 08:12:06 -0300

    add env for password in email(nodemailer)

--- src/services/nodemailer.js
+++ src/services/nodemailer.js
@@ -1,12 +1,14 @@
 const nodemailer = require('nodemailer');
 
+const passEmail = process.env.PWD_EMAIL;
+
 const transport = nodemailer.createTransport({
   service: 'gmail',
   host: 'smtp.gmail.com',
   port: 587,
   auth: {
     user: 'treinamento.ptr@gmail.com',
-    pass: 'htoa steq bpeg psrq',
+    pass: passEmail,
   },
   tls: {
     rejectUnauthorized: false,
